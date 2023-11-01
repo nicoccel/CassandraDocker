ea0478c60b231fdcce0f57bb4a576369b9e5a40e
nicoccel <nicocell@libero.it>
2023-11-01 11:07:02 +0100

    Test per multinode cassandra

--- WINDOWS/build-container-auto.ps1
+++ WINDOWS/build-container-auto.ps1
@@ -1,1 +1,3 @@
-docker run -d --name cassandraBalena -p 9042:9042 -v D:\Multiverso\Cassandra:/var/lib/cassandra cassandra
+docker run --name cassandraBalena -p 9042:9042 -v D:\Multiverso\Cassandra:/var/lib/cassandra -d cassandra
+
+docker run --name cassandraBlu -e CASSANDRA_SEEDS=cassandraBalena -v D:\Multiverso\Cassandra:/var/lib/cassandra -d cassandra
--- WINDOWS/build-container.ps1
+++ WINDOWS/build-container.ps1
@@ -1,5 +1,5 @@
-docker run -d --name cassandraBalena -p 9042:9042 -v cassandraVolume:/var/lib/cassandra cassandra
+docker run --name cassandraBalena -p 9042:9042 -v cassandraVolume:/var/lib/cassandra -d cassandra
 
-#docker run -d --name cassandraBlu -p 9044:9042 -v cassandraVolume:/var/lib/cassandra cassandra
+docker run -d --name cassandraBlu -p 9044:9042 -v cassandraVolume:/var/lib/cassandra -e CASSANDRA_SEEDS=cassandraBalena cassandra
 
 #docker run -d --name cassandraBianco -p 9046:9042 -v cassandraVolume:/var/lib/cassandra cassandra
